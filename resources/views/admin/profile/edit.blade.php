ad4e9663c9f9fb60666651c1fe6dbcdb387b2d25
名前 <>
2019-11-14 11:51:41 +0000

    mynews

---
+++ resources/views/admin/profile/edit.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <style>
+            body {
+                color: #636b6f;
+                letter-spacing: 1px;
+                font-family:"Yu Gothic Medium", "游ゴシック Medium", YuGothic, "游ゴシック体", "ヒラギノ角ゴ Pro W3", "メイリオ", sans-serif;
+            }
+            h1,footer {
+                text-align: center;
+            }
+            header {
+                padding-top: 250px;
+            }
+        </style>
+        <title>profile</title>
+    </head>
+    <body>
+        <header>
+            <h1>profile編集画面</h1>
+        </header>
+        <div class="main">
+        </div>
+        <footer>
+            <small>&copy; 2019 TechBoost PHP/Laravelコース課題</small>
+        </footer>
+    </body>
+</html>
